b8446f19ff65544c6059cf501d0bb12f1da9e3b0
Niki Akbar <nikiakbar@gmail.com>
2026-05-10 21:59:18 +0700

    add provider configurations and Azure authentication secrets

---
+++ terraform/azure/stacks/az-appservice-docker/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/azurerm/latest
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      # version = "4.0.1"
+    }
+    # https://registry.terraform.io/providers/hashicorp/kubernetes/latest
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32.0"
+    }
+  }
+  required_version = ">= 1.9.3"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "kubernetes" {
+  host                   = module.aks.host
+  client_certificate     = base64decode(module.aks.client_certificate)
+  client_key             = base64decode(module.aks.client_key)
+  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+}
